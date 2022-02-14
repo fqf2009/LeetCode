1269f22eec873f099d7c6336b7004e63b0f5f9b2
FrankFeng <fqf2009@gmail.com>
2022-02-14 10:56:02 -0800

    DFS + DP + Memo

--- 0329_LongestIncreasingPathInMatrix.py
+++ 0329_LongestIncreasingPathInMatrix.py
@@ -4,21 +4,41 @@ from typing import List
 # in matrix. From each cell, you can either move in four directions: left, right, 
 # up, or down. You may not move diagonally or move outside the boundary (i.e., 
 # wrap-around is not allowed).
+from functools import cache
 
-# DFS (Depth First Search) and DP (Dynamic Programming)
-# Runtime: 416 ms, faster than 89.17% of Python3 online submissions for Longest Increasing Path in a Matrix.
-# Memory Usage: 15 MB, less than 81.46% of Python3 online submissions for Longest Increasing Path in a Matrix.
+# Use cache decorator to simplify the code
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        def findLongestPath(i, j, depth) -> int:
+        @cache
+        def findLongestPath(i, j) -> int:
+            depth = 1
+            for di, dj in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                x, y = i + di, j + dj
+                if 0 <= x < m and 0 <= y < n and matrix[i][j] < matrix[x][y]:
+                    depth = max(depth, findLongestPath(x, y) + 1)
+            return depth
+
+        m, n = len(matrix), len(matrix[0])
+        maxPath = 0
+        for i in range(m):
+            for j in range(n):
+                maxPath = max(maxPath, findLongestPath(i, j))
+
+        return maxPath
+
+
+# DFS + DP + Memo
+class Solution1:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        def findLongestPath(i, j) -> int:
             if dp[i][j] > 0:
                 return dp[i][j]
+            depth = 1
             m, n = len(matrix), len(matrix[0])
             for di, dj in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
                 x, y = i + di, j + dj
-                if x >= 0 and x < m and y >= 0 and y < n and matrix[x][y] > matrix[i][j]:
-                    # dfs, when next number is increasing
-                    d = findLongestPath(x, y, 1)
+                if 0 <= x < m and 0 <= y < n and matrix[i][j] < matrix[x][y]:
+                    d = findLongestPath(x, y)
                     depth = max(depth, d + 1)
             dp[i][j] = depth
             return depth
@@ -28,21 +48,22 @@ class Solution:
         maxPath = 0
         for i in range(m):
             for j in range(n):
-                maxPath = max(maxPath, findLongestPath(i, j, 1))
+                maxPath = max(maxPath, findLongestPath(i, j))
 
         return maxPath
 
 
 if __name__ == "__main__":
-    sol = Solution()
+    def unit_test(sol):
+        r = sol.longestIncreasingPath(matrix = [[9,9,4],[6,6,8],[2,1,1]])
+        print(r)
+        assert (r == 4)
 
-    r = sol.longestIncreasingPath(matrix = [[9,9,4],[6,6,8],[2,1,1]])
-    print(r)
-    assert (r == 4)
+        r = sol.longestIncreasingPath(matrix = [[3,4,5],[3,2,6],[2,2,1]])
+        print(r)
+        assert (r == 4)
 
-    r = sol.longestIncreasingPath(matrix = [[3,4,5],[3,2,6],[2,2,1]])
-    print(r)
-    assert (r == 4)
-
+    unit_test(Solution())
+    unit_test(Solution1())
 
     
