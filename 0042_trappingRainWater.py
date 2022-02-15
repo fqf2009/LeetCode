9948be6befd0fb3a1945b22bef141e7af64f001d
FrankFeng <fqf2009@gmail.com>
2022-02-14 20:47:19 -0800

    DP (simplify code)

--- 0042_trappingRainWater.py
+++ 0042_trappingRainWater.py
@@ -1,42 +1,39 @@
-from typing import List
-
 # Given n non-negative integers representing an elevation map where the width 
 # of each bar is 1, compute how much water it can trap after raining.
+
+from typing import List
+
 
 # DP (Dynamic Programing): O(n)
 # dpLeft[i] : by far the highest wall to the left
 # dpRight[i]: by far the highest wall to the right
 class Solution:
     def trap(self, height: List[int]) -> int:
-        dpLeft, dpRight = [0] * len(height), [0] * len(height)
-        dp0 = 0
-        for i in range(len(height)):
-            dpLeft[i] = max(dp0, height[i])
-            dp0 = dpLeft[i]
-
-        dp0 = 0
-        for i in reversed(range(len(height))):
-            dpRight[i] = max(dp0, height[i])
-            dp0 = dpRight[i]
+        n = len(height)
+        dpLeft, dpRight = [height[0]] * n, [height[-1]] * n
+        for i in range(1, n-1):
+            dpLeft[i] = max(dpLeft[i-1], height[i])
+            dpRight[n-1-i] = max(dpRight[n-i], height[n-1-i])
 
         res = 0
-        for i in range(1, len(height) - 1):
-            water = min(dpLeft[i], dpRight[i]) - height[i]
-            if water > 0:
-                res += water
+        for i in range(1, n - 1):
+            res += max(min(dpLeft[i], dpRight[i]) - height[i], 0)
 
         return res
 
 
 if __name__ == "__main__":
-    height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    expected = 6
-    r = Solution().trap(height)
-    print(r)
-    assert(r == expected)
+    def unit_test(sol):
+        height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+        expected = 6
+        r = Solution().trap(height)
+        print(r)
+        assert r == expected
 
-    height = [4, 2, 0, 3, 2, 5]
-    expected = 9
-    r = Solution().trap(height)
-    print(r)
-    assert(r == expected)
+        height = [4, 2, 0, 3, 2, 5]
+        expected = 9
+        r = Solution().trap(height)
+        print(r)
+        assert r == expected
+
+    unit_test(Solution())
