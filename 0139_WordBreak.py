4277720f4f830bbdfd9e450367287825f9dc19a7
FrankFeng <fqf2009@gmail.com>
2022-02-20 11:41:06 -0800

    DFS + DP + Memo, String

--- 0139_WordBreak.py
+++ 0139_WordBreak.py
@@ -1,15 +1,31 @@
-from typing import DefaultDict, List
-
 # Given a string s and a dictionary of strings wordDict, return true if s can 
 # be segmented into a space-separated sequence of one or more dictionary words.
 # Note that same word in dictionary may be reused multiple times in segmentation.
+from typing import DefaultDict, List
+from functools import cache
+
+
+# Simplify the code
+# DFS + DP + Memo
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @cache
+        def dfsWordBreak(pos: int) -> bool:
+            if len(s[pos:]) == 0:
+                return True
+            for w in wordDict:
+                if s.startswith(w, pos):
+                    if dfsWordBreak(pos + len(w)):
+                        return True
+            return False
+
+        return dfsWordBreak(0)
+
 
 # DFS + DP + Memorization
 # dp[i]: from position i, whether it is possible to break word using wordDict
 #        value could be: -1 unknown yet, 0 No, 1 yes
-# Runtime: 32 ms, faster than 94.92% of Python3 online submissions for Word Break.
-# Memory Usage: 14.3 MB, less than 71.98% of Python3 online submissions for Word Break.
-class Solution:
+class Solution1:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         def dfsWordBreak(pos: int) -> bool:
             if len(s[pos:]) == 0:
@@ -33,17 +49,18 @@ class Solution:
 
 
 if __name__ == "__main__":
-    sol = Solution()
+    def unitTest(sol):
+        r = sol.wordBreak(s="leetcode", wordDict=["leet", "code"])
+        print(r)
+        assert r == True
 
-    r = sol.wordBreak(s="leetcode", wordDict=["leet", "code"])
-    print(r)
-    assert (r == True)
+        r = sol.wordBreak(s="applepenapple", wordDict=["apple", "pen"])
+        print(r)
+        assert r == True
 
-    r = sol.wordBreak(s="applepenapple", wordDict=["apple", "pen"])
-    print(r)
-    assert (r == True)
+        r = sol.wordBreak(s="catsandog", wordDict=["cats", "dog", "sand", "and", "cat"])
+        print(r)
+        assert r == False
 
-    r = sol.wordBreak(s="catsandog", wordDict=[
-                      "cats", "dog", "sand", "and", "cat"])
-    print(r)
-    assert (r == False)
+    unitTest(Solution())
+    unitTest(Solution1())
