e59998edb3d9d1ea35fcbc43b2cdaf31a67c20ad
FrankFeng <fqf2009@gmail.com>
2022-02-20 16:19:21 -0800

    DFS + DP + Memo

--- 0329_LongestIncreasingPathInMatrix.py
+++ 0329_LongestIncreasingPathInMatrix.py
@@ -1,10 +1,10 @@
-from typing import List
-
 # Given an m x n integers matrix, return the length of the longest increasing path
-# in matrix. From each cell, you can either move in four directions: left, right, 
-# up, or down. You may not move diagonally or move outside the boundary (i.e., 
+# in matrix. From each cell, you can either move in four directions: left, right,
+# up, or down. You may not move diagonally or move outside the boundary (i.e.,
 # wrap-around is not allowed).
+from typing import List
 from functools import cache
+
 
 # Use cache decorator to simplify the code
 class Solution:
@@ -55,15 +55,17 @@ class Solution1:
 
 if __name__ == "__main__":
     def unit_test(sol):
-        r = sol.longestIncreasingPath(matrix = [[9,9,4],[6,6,8],[2,1,1]])
+        r = sol.longestIncreasingPath(matrix=[[9, 9, 4], 
+                                              [6, 6, 8], 
+                                              [2, 1, 1]])
         print(r)
         assert (r == 4)
 
-        r = sol.longestIncreasingPath(matrix = [[3,4,5],[3,2,6],[2,2,1]])
+        r = sol.longestIncreasingPath(matrix=[[3, 4, 5], 
+                                              [3, 2, 6], 
+                                              [2, 2, 1]])
         print(r)
         assert (r == 4)
 
     unit_test(Solution())
     unit_test(Solution1())
-
-    
