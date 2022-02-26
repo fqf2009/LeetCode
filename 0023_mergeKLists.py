ab596c85815b8bf1bc7a6b2d4e1ef1c77ee8b664
FrankFeng <fqf2009@gmail.com>
2022-02-25 18:41:06 -0800

    PriorityQueue v.s. heapq v.s. Merge Sort

--- 0023_mergeKLists.py
+++ 0023_mergeKLists.py
@@ -2,6 +2,7 @@ from lib.ListUtil import ListNode, ListNodeUtil
 from typing import Optional, List, Any
 from queue import PriorityQueue
 from dataclasses import dataclass, field
+import heapq
 
 # You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 # Merge all the linked-lists into one sorted linked-list and return it.
@@ -13,8 +14,7 @@ from dataclasses import dataclass, field
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         def merge2Lists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-            if len(lists) == 1:
-                return lists[0]
+            if len(lists) == 1: return lists[0]     # only have one list
             head = p = ListNode()
             p1, p2 = lists[0], lists[1]
             while p1 and p2:
@@ -22,17 +22,10 @@ class Solution:
                     p.next, p, p1 = p1, p1, p1.next
                 else:
                     p.next, p, p2 = p2, p2, p2.next
-
-            if p1:
-                p.next = p1
-            if p2:
-                p.next = p2
-
+            p.next = p1 if p1 else p2
             return head.next
 
-        if len(lists) == 0:
-            return None
-
+        if len(lists) == 0: return None
         l1 = lists
         while len(l1) > 1:
             l2 = []
@@ -43,17 +36,39 @@ class Solution:
         return l1[0]
 
 
+# Use heapq as PriorityQueue (in Python lib, PriorityQueue is based on heapq)
+# - use a unique sequence as a tie-breaker, because the ListNode does not 
+#   implement the comparison method like __eq__ etc.
+class Solution1:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        que = []
+        seq = 0     # tie-breaker for tuples in Priority Queue
+        for node in lists:
+            if node:
+                seq += 1
+                heapq.heappush(que, (node.val, seq, node))
+
+        head = p = ListNode()
+        while len(que) > 0:
+            _, _, p.next = heapq.heappop(que)
+            p = p.next
+            if p.next:
+                seq += 1
+                heapq.heappush(que, (p.next.val, seq, p.next))
+
+        return head.next
+
+
 @dataclass(order=True)
 class PrioritizedItem:
     priority: int
     node: Any = field(compare=False)
 
-
 # Merge k-Lists using PriorityQueue: put all head nodes from k-Lists into a PriorityQueue,
 # each time pop the smallest node, and push the next node of the poped-up node into queue.
 # Time complexity: O(k*n*log(k)), where n is average list length.
 # Time complexity: O(N*log(k)), where N = k*n, which is the number of nodes from all lists
-class Solution1:
+class Solution2:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         que = PriorityQueue(maxsize=len(lists))
         for node in lists:
@@ -63,7 +78,7 @@ class Solution1:
         head = p = ListNode()
         while not que.empty():
             item = que.get()
-            v, node = item.priority, item.node
+            node = item.node
             p.next = node
             p = node
             if node.next:
@@ -77,7 +92,7 @@ class Solution1:
 # If the data elements are not comparable, the data can be wrapped in a class 
 # that ignores the data item and only compares the priority number.
 # See Solution1.
-class Solution2:
+class Solution3:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         que = PriorityQueue(maxsize=len(lists))
         for node in lists:
@@ -120,3 +135,5 @@ if __name__ == '__main__':
 
     unitTest(Solution())
     unitTest(Solution1())
+    unitTest(Solution2())
+    # unitTest(Solution3())
