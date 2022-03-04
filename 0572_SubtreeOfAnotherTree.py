ccd2f7121b9c31c2aecb2a22dcfef6d41f2794fe
FrankFeng <fqf2009@gmail.com>
2022-03-04 11:47:29 -0800

    DFS + Recursion

--- 0572_SubtreeOfAnotherTree.py
+++ 0572_SubtreeOfAnotherTree.py
@@ -1,10 +1,14 @@
-from typing import Optional
-from lib.TreeUtil import TreeNode, TreeNodeUtil
-
 # Given the roots of two binary trees root and subRoot, return true if there is a
 # subtree of root with same structure and node values of subRoot and false otherwise.
 # A subtree of a binary tree tree is a tree that consists of a node in tree and all of
 # this node's descendants. The tree tree could also be considered as a subtree of itself.
+# Constraints:
+#   The number of nodes in the root tree is in the range [1, 2000].
+#   The number of nodes in the subRoot tree is in the range [1, 1000].
+#   -10^4 <= root.val <= 10^4
+#   -10^4 <= subRoot.val <= 10^4
+from typing import Optional
+from lib.TreeUtil import TreeNode, TreeNodeUtil
 
 
 # DFS + Recursion
@@ -13,17 +17,15 @@ class Solution:
         def matchTree(r1: Optional[TreeNode], r2: Optional[TreeNode]) -> bool:
             if not r1 and not r2:
                 return True
-            elif r1 and r2:
-                return ( r1.val == r2.val and 
+            elif not r1 or not r2:
+                return False
+            return ( r1.val == r2.val and 
                           matchTree(r1.left, r2.left) and 
                           matchTree(r1.right, r2.right) )
-            else:
-                return False
 
-        if root == None or subRoot == None:
-            return subRoot == None
+        if not root or not subRoot: return subRoot is None
 
-        return ((root.val == subRoot.val and matchTree(root, subRoot))
+        return (matchTree(root, subRoot)
                 or self.isSubtree(root.left, subRoot)
                 or self.isSubtree(root.right, subRoot))
 
