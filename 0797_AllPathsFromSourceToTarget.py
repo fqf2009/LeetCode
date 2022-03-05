56c9540dfb335f0da6f854dcd16a59795841cf27
FrankFeng <fqf2009@gmail.com>
2022-03-04 23:38:33 -0800

    DFS + Recursion (DAG)

--- 0797_AllPathsFromSourceToTarget.py
+++ 0797_AllPathsFromSourceToTarget.py
@@ -1,12 +1,37 @@
-from typing import List
-
 # Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find
 # all possible paths from node 0 to node n - 1 and return them in any order.
 # The graph is given as follows: graph[i] is a list of all nodes you can visit 
 # from node i (i.e., there is a directed edge from node i to node graph[i][j]).
 
-# Use stack and recursion to performe DFS (depth first search)
+# Constraints:
+#   n == graph.length
+#   2 <= n <= 15
+#   0 <= graph[i][j] < n
+#   graph[i][j] != i (i.e., there will be no self-loops).
+#   All the elements of graph[i] are unique.
+#   The input graph is guaranteed to be a DAG.
+from typing import List
+
+
+# DFS + Recursion
+# - DAG, no circle, no need to mark visited
+# - creating new list each time, so no need to use stack
 class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        def findPath(start, end, path: List):
+            if start == end:
+                pathes.append(path)
+            else:
+                for node in graph[start]:
+                    findPath(node, end, path + [node])
+
+        pathes = []
+        findPath(0, len(graph) - 1, [0])
+        return pathes
+
+
+# Use stack and recursion to performe DFS (depth first search)
+class Solution1:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         def findPath(start, end):
             if start == end:
@@ -26,24 +51,25 @@ class Solution:
         return res
 
 if __name__ == '__main__':
-    sol = Solution()
+    def unitTest(sol):
+        r = sol.allPathsSourceTarget(graph=[[1, 2], [3], [3], []])
+        print(r)
+        assert(r == [[0, 1, 3], [0, 2, 3]])
 
-    r = sol.allPathsSourceTarget(graph=[[1, 2], [3], [3], []])
-    print(r)
-    assert(r == [[0, 1, 3], [0, 2, 3]])
+        r = sol.allPathsSourceTarget(graph=[[4, 3, 1], [3, 2, 4], [3], [4], []])
+        print(r)
+        assert(r == [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]])
 
-    r = sol.allPathsSourceTarget(graph=[[4, 3, 1], [3, 2, 4], [3], [4], []])
-    print(r)
-    assert(r == [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]])
+        r = sol.allPathsSourceTarget(graph=[[1], []])
+        print(r)
+        assert(r == [[0, 1]])
 
-    r = sol.allPathsSourceTarget(graph=[[1], []])
-    print(r)
-    assert(r == [[0, 1]])
+        r = sol.allPathsSourceTarget(graph=[[1, 2, 3], [2], [3], []])
+        print(r)
+        assert(r == [[0, 1, 2, 3], [0, 2, 3], [0, 3]])
 
-    r = sol.allPathsSourceTarget(graph=[[1, 2, 3], [2], [3], []])
-    print(r)
-    assert(r == [[0, 1, 2, 3], [0, 2, 3], [0, 3]])
+        r = sol.allPathsSourceTarget(graph=[[1, 3], [2], [3], []])
+        print(r)
+        assert(r == [[0, 1, 2, 3], [0, 3]])
 
-    r = sol.allPathsSourceTarget(graph=[[1, 3], [2], [3], []])
-    print(r)
-    assert(r == [[0, 1, 2, 3], [0, 3]])
+    unitTest(Solution())
