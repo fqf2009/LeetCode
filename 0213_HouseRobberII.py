61d4e989cacc8ccf8cd9ed0778a9cb944e712d1e
FrankFeng <fqf2009@gmail.com>
2022-03-09 14:57:02 -0800

    DP (Recursion vs. Iteration)

--- 0213_HouseRobberII.py
+++ 0213_HouseRobberII.py
@@ -8,19 +8,44 @@
 # Given an integer array nums representing the amount of money of each
 # house, return the maximum amount of money you can rob tonight without
 # alerting the police.
-from typing import List
 
-# DP + Iteration - T/S: O(n), O(1)
+# Constraints:
+#   1 <= nums.length <= 100
+#   0 <= nums[i] <= 1000
+from typing import List
+from functools import cache
+
+
+# DP + Recursion - T/S: O(n), O(n)
 # dp[i] = max value at position i
 # dp[i] only rely on dp[i-1] and dp[i-2]
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def robNoCircle(nums: List[int]) -> int:
-            dp2 = dp1 = dp0 = nums[0]
-            if len(nums) > 1:
-                dp2 = dp1 = max(nums[:2])
-            for i in range(2, len(nums)):
-                dp2 = max(dp1, dp0 + nums[i])
+        def robNoCircle(N: List[int]) -> int:
+            @cache
+            def dp(i):
+                if i == 0: return N[0]
+                if i == 1: return max(N[:2])
+                return max(dp(i-1), dp(i-2) + N[i])
+
+            return dp(len(N) - 1)
+
+        if len(nums) == 1:
+            return nums[0]
+        return max(robNoCircle(nums[1:]), robNoCircle(nums[:-1]))
+
+
+# DP + Iteration - T/S: O(n), O(1)
+# dp[i] = max value at position i
+# dp[i] only rely on dp[i-1] and dp[i-2]
+class Solution1:
+    def rob(self, nums: List[int]) -> int:
+        def robNoCircle(N: List[int]) -> int:
+            dp2 = dp1 = dp0 = N[0]
+            if len(N) > 1:
+                dp2 = dp1 = max(N[:2])
+            for i in range(2, len(N)):
+                dp2 = max(dp1, dp0 + N[i])
                 dp1, dp0 = dp2, dp1
             return dp2
 
@@ -48,3 +73,4 @@ if __name__ == '__main__':
         assert r == 18
 
     unitTest(Solution())
+    unitTest(Solution1())
