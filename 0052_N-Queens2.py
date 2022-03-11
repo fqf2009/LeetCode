e50e4cd9dba085b223a3921e3ea5522bc23a7b22
FrankFeng <fqf2009@gmail.com>
2022-03-11 12:21:21 -0800

    Backtracking - Recursion v.s. Iteration

--- 0052_N-Queens2.py
+++ 0052_N-Queens2.py
@@ -1,10 +1,37 @@
-from typing import List
-
 # The n-queens puzzle is the problem of placing n queens on an n x n chessboard
 # such that no two queens attack each other. Given an integer n, return the number
 # of distinct solutions to the n-queens puzzle.
+from typing import List
 
+
+# Backtracking + Recursion: T/S: O(n^3), O(n)
+# - backtrack template
 class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = [0]
+        solution = [-1] * n
+        filledColumns = set()
+        def backtrack(row):
+            if row == n:
+                res[0] += 1
+                return
+            for col in range(n):
+                if col not in filledColumns:
+                    for i in range(row):
+                        if abs(col - solution[i]) == row - i:
+                            break
+                    else:
+                        solution[row] = col
+                        filledColumns.add(col)
+                        backtrack(row + 1)
+                        filledColumns.remove(col)
+
+        backtrack(0)
+        return res[0]     
+
+
+# Backtracking + Iteration
+class Solution1:
     def totalNQueens(self, n: int) -> int:
         def validPos(i, curPos) -> bool:
             for j in range(i):
@@ -39,12 +66,14 @@ class Solution:
 
 
 if __name__ == '__main__':
-    sol = Solution()
+    def unitTest(sol):
+        r = sol.totalNQueens(4)
+        print(r)
+        assert r == 2
 
-    r = sol.totalNQueens(4)
-    print(r)
-    assert(r == 2)
+        r = sol.totalNQueens(1)
+        print(r)
+        assert r == 1
 
-    r = sol.totalNQueens(1)
-    print(r)
-    assert(r == 1)
+    unitTest(Solution())
+    unitTest(Solution1())
