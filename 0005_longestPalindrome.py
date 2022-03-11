ece60406cdd6a7177e5927f985b390492523e0c6
FrankFeng <fqf2009@gmail.com>
2022-03-11 10:22:30 -0800

    palindromic substring (two pointers)

--- 0005_longestPalindrome.py
+++ 0005_longestPalindrome.py
@@ -1,15 +1,32 @@
 # Given a string s, return the longest palindromic substring in s
+# Constraints:
+#   1 <= s.length <= 1000
+#   s consist of only digits and English letters.
 
 # There are much efficient way: Manacher's algorithm: O(n)
 # https://en.wikipedia.org/wiki/Longest_palindromic_substring
 
 
-# use two pointers to point to the center of palindrome, this works for
-# both odd or even length of palindrome strings.
-# Time complexity: O(n^2)
-
-# slight change: just use poiner, defer string manipulation until return
+# - simplify code, i.e., let python do string comparison
+# - Time Limited Exceeded - guess python did extra work during string slicing
 class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        p1 = p2 = 0
+        maxLen = 1
+        for i in range(n-1):
+            for j in range(i + maxLen, n):
+                k = (j - i) // 2
+                if s[i:i+k+1] == s[j:j-k-1:-1]:
+                    maxLen = j - i + 1
+                    p1, p2 = i, j
+
+        return s[p1: p2+1]
+
+
+# Time complexity: O(n^2)
+# slight change: just use poiner, defer string manipulation until return
+class Solution1:
     def longestPalindrome(self, s: str) -> str:
         def maxPallindromeAt(p1: int, p2: int):
             pdLen = 0
@@ -29,9 +46,10 @@ class Solution:
                 
         return s[pdStart: pdStart + maxLen]
 
-
+# use two pointers to point to the center of palindrome, this works for
+# both odd or even length of palindrome strings.
 # Expand around the center: T/S: O(n^2), O(1)
-class Solution1:
+class Solution2:
     def longestPalindrome(self, s: str) -> str:
         # p1, p2 pointing to the center of palindrome
         def palindromeLength(p1: int, p2: int) -> int:
@@ -58,6 +76,10 @@ class Solution1:
 
 if __name__ == "__main__":
     def unitTest(sol):
+        r = Solution().longestPalindrome("eabcb")
+        print(r)
+        assert(r == 'bcb')
+
         r = Solution().longestPalindrome('babad')
         print(r)
         assert(r == 'bab' or r == 'aba')
@@ -76,3 +98,4 @@ if __name__ == "__main__":
 
     unitTest(Solution())
     unitTest(Solution1())
+    unitTest(Solution2())
