52c570aa8ac3591fc3171f5599e367619e1e70a9
FrankFeng <fqf2009@gmail.com>
2022-03-10 23:15:51 -0800

    PriorityQueue (heapq) v.s. deque (Monotonic)

--- 0239_SlidingWindowMaximum.py
+++ 0239_SlidingWindowMaximum.py
@@ -20,17 +20,37 @@
 #    1  3  -1  -3 [5  3  6] 7       6
 #    1  3  -1  -3  5 [3  6  7]      7
 from typing import List
+from collections import deque
 import heapq
+
+
+# Deque: T/S: O(n), O(k)
+# - ensure the deque window only has decreasing elements.
+#   That way, the leftmost element is always the largest.
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+        res = []
+        for i in range(len(nums)):
+            while dq and dq[0] <= i - k:
+                dq.popleft()
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+
+        return res
 
 
 # PriorityQueue (HeapQueue)
 # T/S: O(n*log(k)), O(k)
-class Solution:
+class Solution1:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        window = [(-x, i) for i, x in enumerate(nums[:k+1])]
+        window = [(-x, i) for i, x in enumerate(nums[:k])]
         heapq.heapify(window)
         res = []
-        for i in range(0, len(nums) - k + 1):
+        for i in range(len(nums) - k + 1):
             while window[0][1] < i:
                 heapq.heappop(window)
             res.append(-window[0][0])
@@ -42,6 +62,10 @@ class Solution:
 
 if __name__ == '__main__':
     def unitTest(sol):
+        r = sol.maxSlidingWindow([2, 7, 8], 2)
+        print(r)
+        assert r == [7, 8]
+
         r = sol.maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3)
         print(r)
         assert r == [3, 3, 5, 5, 6, 7]
