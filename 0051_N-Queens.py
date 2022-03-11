9ded69680fa447f37dff0896d155864fe2a55dff
FrankFeng <fqf2009@gmail.com>
2022-03-11 12:14:38 -0800

    Backtracking - Iteration v.s. Recursion

--- 0051_N-Queens.py
+++ 0051_N-Queens.py
@@ -1,12 +1,47 @@
-from typing import List
-
 # The n-queens puzzle is the problem of placing n queens on an n x n chessboard
 # such that no two queens attack each other. Given an integer n, return all
 # distinct solutions to the n-queens puzzle. You may return the answer in any order.
 # Each solution contains a distinct board configuration of the n-queens' placement,
 # where 'Q' and '.' both indicate a queen and an empty space, respectively.
+# Constraints:
+#   1 <= n <= 9
+from typing import List
 
+
+# Backtracking + Recursion: T/S: O(n^3), O(n)
+# - backtrack template
 class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def createBoard(solution: list[int]) -> list[str]:
+            res = []
+            for v in solution:
+                res.append('.' * v + 'Q' + '.' * (n - v - 1))
+            return res
+
+        res = []
+        solution = [-1] * n
+        filledColumns = set()
+        def backtrack(row):
+            if row == n:
+                res.append(createBoard(solution))
+                return
+            for col in range(n):
+                if col not in filledColumns:
+                    for i in range(row):
+                        if abs(col - solution[i]) == row - i:
+                            break
+                    else:
+                        solution[row] = col
+                        filledColumns.add(col)
+                        backtrack(row + 1)
+                        filledColumns.remove(col)
+
+        backtrack(0)
+        return res        
+
+
+# Backtracking + Iteration
+class Solution1:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def validPos(i, curPos) -> bool:
             for j in range(i):
@@ -51,12 +86,14 @@ class Solution:
 
 
 if __name__ == '__main__':
-    sol = Solution()
+    def unitTest(sol):
+        r = sol.solveNQueens(4)
+        print(r)
+        assert(r == [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]])
 
-    r = sol.solveNQueens(4)
-    print(r)
-    assert(r == [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]])
+        r = sol.solveNQueens(1)
+        print(r)
+        assert(r == [["Q"]])
 
-    r = sol.solveNQueens(1)
-    print(r)
-    assert(r == [["Q"]])
+    unitTest(Solution())
+    unitTest(Solution1())
