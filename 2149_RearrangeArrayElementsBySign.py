2302bb1c714a84c387b789de9c3b4d9cd6372450
FrankFeng <fqf2009@gmail.com>
2022-03-12 13:40:52 -0800

    Two Pointers

--- 2149_RearrangeArrayElementsBySign.py
+++ 2149_RearrangeArrayElementsBySign.py
@@ -6,12 +6,28 @@
 #  - For all integers with the same sign, the order in which they were present in nums is preserved.
 #  - The rearranged array begins with a positive integer.
 # Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
-
 from typing import List
 
 
+# Two Pointers
 # Time complexity: O(n), space complexity: O(n)
 class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        pp, pn = 0, 1   # current pos of postive and negative number in res
+        for v in nums:
+            if v > 0:
+                res[pp] = v
+                pp += 2
+            else:
+                res[pn] = v
+                pn += 2
+
+        return res
+
+
+# Time complexity: O(n), space complexity: O(n)
+class Solution1:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         pos = [x for x in nums if x > 0]
         neg = [x for x in nums if x < 0]
@@ -32,3 +48,4 @@ if __name__ == '__main__':
         assert(r == [1,-1])
 
     unitTest(Solution())
+    unitTest(Solution1())
