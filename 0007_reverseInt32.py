02296c977fdd0818d971f04236310d35b8090dc2
FrankFeng <fqf2009@gmail.com>
2022-03-11 20:14:35 -0800

    string or math

--- 0007_reverseInt32.py
+++ 0007_reverseInt32.py
@@ -2,8 +2,15 @@
 # the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
 # Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
-
 class Solution:
+    def reverse(self, x: int) -> int:
+        res = int(str(x)[::-1]) if x >= 0 else -self.reverse(-x)
+        if res > 2**31 - 1 or res < -(2**31):
+            return 0
+        return res
+
+
+class Solution1:
     def reverse(self, x: int) -> int:
         if x < 0:
             return -self.reverse(-x)
@@ -18,25 +25,28 @@ class Solution:
         
         return res
 
+
 if __name__ == '__main__':
-    sol = Solution()
+    def unitTest(sol):
+        n = sol.reverse(123)
+        print(n)
+        assert n == 321
 
-    n = sol.reverse(123)
-    print(n)
-    assert(n == 321)
+        n = sol.reverse(-123)
+        print(n)
+        assert n == -321
 
-    n = sol.reverse(-123)
-    print(n)
-    assert(n == -321)
+        n = sol.reverse(120)
+        print(n)    
+        assert n == 21
 
-    n = sol.reverse(120)
-    print(n)    
-    assert(n == 21)
+        n = sol.reverse(8463847412)
+        print(n)
+        assert n == 0
 
-    n = sol.reverse(8463847412)
-    print(n)
-    assert(n == 0)
+        n = sol.reverse(-8463847412)
+        print(n)
+        assert n == 0
 
-    n = sol.reverse(-8463847412)
-    print(n)
-    assert(n == 0)
+    unitTest(Solution())
+    unitTest(Solution1())
