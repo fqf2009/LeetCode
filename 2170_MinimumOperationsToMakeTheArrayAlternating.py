a51409e4931b7d25acba7359a8739b2cef01f552
FrankFeng <fqf2009@gmail.com>
2022-03-12 15:22:14 -0800

    Counter

--- 2170_MinimumOperationsToMakeTheArrayAlternating.py
+++ 2170_MinimumOperationsToMakeTheArrayAlternating.py
@@ -4,28 +4,40 @@
 #   nums[i - 1] != nums[i], where 1 <= i <= n - 1.
 # In one operation, you can choose an index i and change nums[i] 
 # into any positive integer.
-
 # Return the minimum number of operations required to make the array 
 # alternating.
+# Example 1:
+#   Input: nums = [3,1,3,2,4,3]
+#   Output: 3
+#   Explanation:
+#     One way to make the array alternating is by converting it to [3,1,3,1,3,1].
+#     The number of operations required in this case is 3.
+#     It can be proven that it is not possible to make the array alternating in
+#     less than 3 operations. 
+# Example 2:
+#   Input: nums = [1,2,2,2,2]
+#   Output: 2
+#   Explanation:
+#   One way to make the array alternating is by converting it to [1,2,1,2,1].
+#   The number of operations required in this case is 2.
+#   Note that the array cannot be converted to [2,2,2,2,2] because in this case
+#   nums[0] == nums[1] which violates the conditions of an alternating array.
 
 # Constraints:
 #   1 <= nums.length <= 10^5
 from typing import List
 from collections import Counter
 
+# Counter: T/S: O(n), O(n)
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return 0
-
-        c1 = Counter(nums[::2])
-        c2 = Counter(nums[1::2])
-        freq1 = sorted(c1.items(), key=lambda x: -x[1])
-        freq2 = sorted(c2.items(), key=lambda x: -x[1])
+        if n == 1: return 0
+        c1 = Counter(nums[::2]).most_common(2)
+        c2 = Counter(nums[1::2]).most_common(2)
         res = n
-        for v1, f1 in freq1[:2]:
-            for v2, f2 in freq2[:2]:
+        for v1, f1 in c1:
+            for v2, f2 in c2:
                 if v1 != v2:
                     res = min(res, n - f1 - f2)
                 else:
