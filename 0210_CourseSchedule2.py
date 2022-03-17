75f51f65b6d86ac7524b9aecf1da93e1a64fdfce
FrankFeng <fqf2009@gmail.com>
2022-03-17 16:25:28 -0700

    Directed Graph and Cycle Detection

--- 0210_CourseSchedule2.py
+++ 0210_CourseSchedule2.py
@@ -1,6 +1,3 @@
-from typing import List
-from collections import defaultdict
-
 # There are a total of numCourses courses you have to take, labeled 
 # from 0 to numCourses - 1. You are given an array prerequisites where 
 # prerequisites[i] = [ai, bi] indicates that you must take course bi 
@@ -10,49 +7,64 @@ from collections import defaultdict
 # If there are many valid answers, return any of them. If it is 
 # impossible to finish all courses, return an empty array.
 
-# DFS (Depth First Search) to sort DG (Directed Graph) and detect cycle
-class Solution1:
+# Constraints:
+#   1 <= numCourses <= 2000
+#   0 <= prerequisites.length <= numCourses * (numCourses - 1)
+#   prerequisites[i].length == 2
+#   0 <= ai, bi < numCourses
+#   ai != bi
+#   All the pairs [ai, bi] are distinct.
+from typing import List
+from collections import defaultdict
+
+
+# DFS - to sort DG (Directed Graph) and detect cycle
+# Time:  O(V+E), where V is number of vertices (vertex)
+# Space: O(V+E)
+class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         def dfsVisitNoCycle(node) -> bool:
-            if visited[node] == 1:
+            if visited[node] == 1:  # previously visited in another search
                 return True
-            if visited[node] == -1:
+            if visited[node] == -1: # previously visited in this search, cycle detected
                 return False
-            visited[node] = -1
+            visited[node] = -1      # mark visiting in this search
             for x in graph[node]:
                 if not dfsVisitNoCycle(x):
                     return False
-            visited[node] = 1
+            visited[node] = 1       # mark visited
             visitOrder.append(node)
             return True
 
         graph = defaultdict(set)
-        for req in prerequisites:
-            graph[req[0]].add(req[1])
+        for course, prereq in prerequisites:    # O(E)
+            graph[course].add(prereq)
 
         # 0: not visited, 1: visited, -1: in DFS searching stack
         visited = [0] * numCourses
         visitOrder = []
-        for i in range(len(visited)):
-            if not dfsVisitNoCycle(i):
+        for i in range(len(visited)):           # O(V)
+            if visited[i] == 0 and not dfsVisitNoCycle(i):
                 return []
 
         return visitOrder
 
 
-# BFS (Breadth First Search) to sort DG (Directed Graph) and detect cycle
-class Solution:
+# BFS and Node Indegree - to sort DG (Directed Graph) and detect cycle
+# Time:  O(V+E), where V is number of vertices (vertex)
+# Space: O(V+E)
+class Solution1:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(set)
         indegree = {}
-        for req in prerequisites:
-            indegree.setdefault(req[1], 0)
-            indegree[req[1]] += 1
-            graph[req[0]].add(req[1])
+        for course, prereq in prerequisites:    # O(E)
+            graph[course].add(prereq)
+            indegree.setdefault(prereq, 0)
+            indegree[prereq] += 1
 
         toVisit = set(range(numCourses)) - set(indegree.keys())
         visitOrder = []
-        while len(toVisit) > 0:
+        while len(toVisit) > 0:                 # O(V)
             node = toVisit.pop()
             visitOrder.append(node)
             for x in graph[node]:
@@ -67,15 +79,14 @@ class Solution:
 
 
 if __name__ == '__main__':
-    sol = Solution()
+    def unitTest(sol):
+        r = sol.findOrder(numCourses=2, prerequisites=[[1, 0]])
+        print(r)
+        assert r == [0,1]
 
-    r = sol.findOrder(numCourses=2, prerequisites=[[1, 0]])
-    print(r)
-    assert(r == [0,1])
+        r = sol.findOrder(numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]])
+        print(r)
+        assert r == [0,1,2,3] or r == [0,2,1,3]
 
-    r = sol.findOrder(numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]])
-    print(r)
-    assert(r == [0,1,2,3] or r == [0,2,1,3])
-
-
-        
+    unitTest(Solution())
+    unitTest(Solution1())
