fe82b8198a4292b6591d703a691b81643ae78b2c
FrankFeng <fqf2009@gmail.com>
2022-03-30 18:24:20 -0700

    Binary Tree + DFS

--- 1644_LowestCommonAncestorOfBinaryTreeII.py
+++ 1644_LowestCommonAncestorOfBinaryTreeII.py
@@ -1,21 +1,58 @@
-# Given a binary tree, find the lowest common ancestor (LCA) of two given 
-# nodes in the tree.
-# According to the definition of LCA on Wikipedia: “The lowest common 
-# ancestor is defined between two nodes p and q as the lowest node in 
-# T that has both p and q as descendants (where we allow a node to be 
-# a descendant of itself).”
+# Given the root of a binary tree, return the lowest common ancestor (LCA)
+# of two given nodes, p and q. If either node p or q does not exist in the 
+# tree, return null. All values of the nodes in the tree are unique.
+#
+# According to the definition of LCA on Wikipedia: "The lowest common 
+# ancestor of two nodes p and q in a binary tree T is the lowest node 
+# that has both p and q as descendants (where we allow a node to be a 
+# descendant of itself)". A descendant of a node x is a node y that is
+# on the path from node x to some leaf node.
 # Constraints:
-#   The number of nodes in the tree is in the range [2, 10^5].
+#   The number of nodes in the tree is in the range [1, 10^4].
 #   -10^9 <= Node.val <= 10^9
 #   All Node.val are unique.
 #   p != q
-#   p and q will exist in the tree.
 from typing import Optional
 from lib.TreeUtil import TreeNode, TreeNodeUtil
 
 
 # Binary Tree + DFS - T/S: O(n), O(n)
+# - Alternative approach, refer to: 1676_LowestCommonAncestorOfBinaryTreeIV
+#   not that good like 1678 one, because nodes might not be in tree.
 class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', 
+                             p: 'TreeNode', q: 'TreeNode') -> Optional['TreeNode']:
+        target = set([p, q])
+        found = []
+        def dfsLCA(root: Optional[TreeNode]) -> Optional[TreeNode]:
+            if root:
+                leftLCA = dfsLCA(root.left)
+                if len(found) == 2:
+                    return leftLCA      # to improve performance
+
+                rightLCA = dfsLCA(root.right)
+                if leftLCA and rightLCA:    # check this before the following one
+                    return root
+
+                # if len(found) == 2:         # no need, and must first check the above one
+                #     return rightLCA
+
+                if root in target:
+                    found.append(root)
+                    return root
+
+                if leftLCA: return leftLCA
+
+                if rightLCA: return rightLCA
+
+            return None
+
+        lca = dfsLCA(root)
+        return lca if len(found) == 2 else None
+
+
+# Binary Tree + DFS - T/S: O(n), O(n)
+class Solution1:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         path = []
         target = [p, q]
@@ -38,7 +75,7 @@ class Solution:
 
             if dfsLCA(root.left) or dfsLCA(root.right):
                 return True
-            
+
             path.pop()
             if len(target) == 1:
                 res[0] = min(res[0], len(path)-1)
@@ -73,3 +110,4 @@ if __name__ == '__main__':
         assert r is None
 
     unitTest(Solution())
+    unitTest(Solution1())
