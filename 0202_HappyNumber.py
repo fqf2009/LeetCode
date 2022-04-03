67e31a70d34ccf5ffa9579214acbbda6f18f7497
FrankFeng <fqf2009@gmail.com>
2022-04-03 09:36:04 -0700

    Math

--- 0202_HappyNumber.py
+++ 0202_HappyNumber.py
@@ -16,6 +16,20 @@ class Solution:
             return True
         nums = {n}
         while True:
+            n = sum(int(x)**2 for x in str(n))
+            if n == 1:
+                return True
+            if n in nums:
+                return False
+            nums.add(n)
+
+
+class Solution1:
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+        nums = {n}
+        while True:
             total = 0
             while n != 0:
                 n, r = divmod(n, 10)
@@ -29,10 +43,18 @@ class Solution:
 
 
 if __name__ == '__main__':
-    r = Solution().isHappy(19)
-    print(r)
-    assert(r == True)
+    def unit_test(sol):
+        r = Solution().isHappy(19)
+        print(r)
+        assert(r == True)
 
-    r = Solution().isHappy(2)
-    print(r)
-    assert(r == False)
+        r = Solution().isHappy(2)
+        print(r)
+        assert(r == False)
+
+        r = Solution().isHappy(45)
+        print(r)
+        assert(r == False)
+
+    unit_test(Solution())
+    unit_test(Solution1())
