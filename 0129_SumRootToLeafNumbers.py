d14a46f7d59c9e8e3c63e87ef070cc9fd180711a
FrankFeng <fqf2009@gmail.com>
2022-04-05 09:03:58 -0700

    Tree, Recursion, DFS

--- 0129_SumRootToLeafNumbers.py
+++ 0129_SumRootToLeafNumbers.py
@@ -1,25 +1,44 @@
-from typing import List, Optional
-from lib.TreeUtil import TreeNode, TreeNodeUtil
-
 # You are given the root of a binary tree containing digits from 0 to 9 only.
 # Each root-to-leaf path in the tree represents a number.
 # For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
-# Return the total sum of all root-to-leaf numbers. Test cases are generated 
+# Return the total sum of all root-to-leaf numbers. Test cases are generated
 # so that the answer will fit in a 32-bit integer.
 # A leaf node is a node with no children.
+# Constraints:
+#   The number of nodes in the tree is in the range [1, 1000].
+#   0 <= Node.val <= 9
+#   The depth of the tree will not exceed 10.
+from typing import Optional
+from lib.TreeUtil import TreeNode, TreeNodeUtil
 
-# Recursion, DFS
+
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def dfsSumNumbers(node: TreeNode, pathVal: int) ->int:
-            pathVal = pathVal * 10 + node.val
+        def dfs_sum(node: Optional[TreeNode], path_val: int) -> int:
+            if not node:
+                return 0
+            path_val = path_val * 10 + node.val
+
+            if node.left or node.right:
+                return dfs_sum(node.left, path_val) + dfs_sum(node.right, path_val)
+            else:
+                return path_val
+
+        return dfs_sum(root, 0)
+
+
+# Tree, Recursion, DFS: O(n)
+class Solution1:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def dfsSumNumbers(node: TreeNode, path_val: int) -> int:
+            path_val = path_val * 10 + node.val
             res1, res2 = 0, 0
             if node.left == None and node.right == None:
-                return pathVal
+                return path_val
             if node.left:
-                res1 = dfsSumNumbers(node.left, pathVal)
+                res1 = dfsSumNumbers(node.left, path_val)
             if node.right:
-                res2 = dfsSumNumbers(node.right, pathVal)
+                res2 = dfsSumNumbers(node.right, path_val)
             return res1 + res2
 
         if not root:
@@ -27,15 +46,17 @@ class Solution:
         return dfsSumNumbers(root, 0)
 
 
-if __name__ == '__main__':
-    sol = Solution()
+if __name__ == "__main__":
 
-    r1 = TreeNodeUtil.fromBfsList([1,2,3])
-    s = sol.sumNumbers(r1)
-    print(s)
-    assert(s == 25)
+    def unit_test(sol):
+        r1 = TreeNodeUtil.fromBfsList([1, 2, 3])
+        s = sol.sumNumbers(r1)
+        print(s)
+        assert s == 25
 
-    r1 = TreeNodeUtil.fromBfsList([4,9,0,5,1])
-    s = sol.sumNumbers(r1)
-    print(s)
-    assert(s == 1026)
+        r1 = TreeNodeUtil.fromBfsList([4, 9, 0, 5, 1])
+        s = sol.sumNumbers(r1)
+        print(s)
+        assert s == 1026
+
+    unit_test(Solution())
