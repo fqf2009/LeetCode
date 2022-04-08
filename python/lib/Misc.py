43ec269b886c588492c35b67083ce10608ee2dc3
FrankFeng <fqf2009@gmail.com>
2022-04-08 13:56:31 -0700

    lib

---
+++ python/lib/Misc.py
@@ -0,0 +1,132 @@
+from typing import List
+from math import sqrt
+
+
+def getPrimes(n: int) -> List[int]:
+    primes = [2]
+    for i in range(3, n + 1):
+        for p in primes:
+            if p * p > i:
+                primes.append(i)
+                break
+            if i % p == 0:
+                break
+    return primes
+
+
+# too slow, calculate all prime is unnecessary
+def getFactors2(n: int) -> List[int]:
+    fac = []
+    primes = getPrimes(int(sqrt(n))+1)
+    i, m = 0, n
+    while m > 1 and i < len(primes):
+        if m % primes[i] == 0:
+            fac.append(primes[i])
+            m = m // primes[i]
+        else:
+            i += 1
+
+    if m > 1:
+        fac.append(m)
+    return fac
+
+
+def getFactors(n: int) -> List[int]:
+    m, i = n, 2
+    fac = []
+    while m > 1 and i <= n**0.5 + 1:
+        if m % i == 0:
+            fac.append(i)
+            m //= i
+        else:
+            i += 1
+
+    if m > 1:
+        fac.append(m)
+    return fac
+
+
+def getDivisors(n: int) -> List[int]:
+    div = []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            div.append(i)
+        if i * i < n:
+            div.append(n // i)
+        i += 1
+    return div
+
+
+def base62Encode(n: int, minlen: int = 1) -> str:
+    charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    chs = []
+    while n > 0:
+        n, r = divmod(n, 62)
+        chs.append(charset[r])
+
+    if len(chs) == 0:
+        s = '0'
+    else:
+        s = ''.join(chs[::-1])
+
+    return charset[0] * max(minlen - len(s), 0) + s
+
+
+def base62Decode(s: str) -> int:
+    res = 0
+    for ch in s:
+        if '0' <= ch <= '9':
+            v = ord(ch) - ord('0')
+        elif 'A' <= ch <= 'Z':
+            v = ord(ch) - ord('A') + 10
+        elif 'a' <= ch <= 'z':
+            v = ord(ch) - ord('a') + 36
+        else:
+            raise ValueError(f'{s} is not a valid Base 62 string!')
+        res = res*62 + v
+
+    return res
+
+
+if __name__ == '__main__':
+    r = getPrimes(100)
+    print(r)
+    assert r == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+                 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+    r = getFactors(100)
+    print(r)
+    assert r == [2, 2, 5, 5]
+
+    r = getFactors(7591)
+    print(r)
+    assert r == [7591]
+
+    r = getFactors2(100)
+    print(r)
+    assert r == [2, 2, 5, 5]
+
+    r = getFactors2(7591)
+    print(r)
+    assert r == [7591]
+
+    r = sorted(getDivisors(12))
+    print(r)
+    assert r == [1, 2, 3, 4, 6, 12]
+
+    r = base62Encode(123456789)
+    print(r)
+    assert r == '8M0kX'
+
+    r = base62Encode(34441886726)
+    print(r)
+    assert r == 'base62'
+
+    r = base62Decode('8M0kX')
+    print(r)
+    assert r == 123456789
+
+    r = base62Decode('base62')
+    print(r)
+    assert r == 34441886726
---
+++ python/lib/TreeUtil.py
@@ -0,0 +1,178 @@
+from collections import deque
+from typing import Optional
+
+# Some utility functions to facilitate the code testing of the algorithm using trees
+
+
+class TreeNode:
+    def __init__(self, val=-1, left=None, right=None, parent=None):
+        self.val = val
+        self.left: Optional[TreeNode] = left
+        self.right: Optional[TreeNode] = right
+        self.parent: Optional[TreeNode] = parent
+
+
+class TreeNodeUtil:
+    # Depth First Search (Traversal)
+    @staticmethod
+    def toInorderList(root: Optional[TreeNode]) -> list:
+        if root is None:
+            return []
+        res = TreeNodeUtil.toInorderList(root.left)
+        res.append(root.val)
+        res.extend(TreeNodeUtil.toInorderList(root.right))
+        return res
+
+    @staticmethod
+    def toPreorderList(root: Optional[TreeNode]) -> list:
+        if root == None:
+            return []
+        res = [root.val]
+        res.extend(TreeNodeUtil.toPreorderList(root.left))
+        res.extend(TreeNodeUtil.toPreorderList(root.right))
+        return res
+
+    @staticmethod
+    def toPostorderList(root: Optional[TreeNode]) -> list[int]:
+        if root == None:
+            return []
+        res = TreeNodeUtil.toPostorderList(root.left)
+        res.extend(TreeNodeUtil.toPostorderList(root.right))
+        res.append(root.val)
+        return res
+
+    @staticmethod
+    def dfsFind(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root == None:
+            return None
+        if root.val == val:
+            return root
+        node = TreeNodeUtil.dfsFind(root.left, val)
+        return node if node else TreeNodeUtil.dfsFind(root.right, val)
+
+    # Breadth First Search (Traversal)
+    @staticmethod
+    def toBfsList(root: Optional[TreeNode]) -> list[Optional[int]]:
+        res = []
+        if root == None:
+            return res
+
+        dq = deque()
+        dq.append(root)
+        res.append(root.val)
+        while len(dq) > 0:
+            node = dq.popleft()
+            if node.left == None:
+                res.append(None)
+            else:
+                dq.append(node.left)
+                res.append(node.left.val)
+
+            if node.right == None:
+                res.append(None)
+            else:
+                dq.append(node.right)
+                res.append(node.right.val)
+
+        # remove trailing None
+        for i in reversed(range(len(res))):
+            if res[i] != None:
+                break
+            res.pop()
+
+        return res
+
+    @staticmethod
+    def fromBfsList(nums: list[Optional[int]]) -> Optional[TreeNode]:
+        if len(nums) == 0:
+            return None
+
+        root = TreeNode(nums[0])
+        dq = deque()
+        dq.append(root)
+        isLeft = True
+        for v in nums[1:]:
+            node = dq[0]
+            if isLeft:
+                if v != None:
+                    node.left = TreeNode(val=v, parent=node)
+                    dq.append(node.left)
+                isLeft = False
+            else:
+                if v != None:
+                    node.right = TreeNode(val=v, parent=node)
+                    dq.append(node.right)
+                isLeft = True
+                dq.popleft()
+
+        return root
+
+    # Create a Binary Search Tree
+    @staticmethod
+    def fromListToBST(nums: list[int]) -> Optional[TreeNode]:
+        def addToBST(root: TreeNode, v: int):
+            if v < root.val:
+                if root.left == None:
+                    root.left = TreeNode(val=v, parent=root)
+                else:
+                    addToBST(root.left, v)
+            else:
+                if root.right == None:
+                    root.right = TreeNode(val=v, parent=root)
+                else:
+                    addToBST(root.right, v)
+
+        if len(nums) == 0:
+            return None
+        root = TreeNode(nums[0])
+        for i in range(1, len(nums)):
+            addToBST(root, nums[i])
+        return root
+
+
+if __name__ == "__main__":
+    # test case
+    root = TreeNodeUtil.fromBfsList([1, None, 2, 3])
+    r = TreeNodeUtil.toInorderList(root)
+    print(r)
+    assert(r == [1, 3, 2])
+
+    root = TreeNodeUtil.fromBfsList([])
+    r = TreeNodeUtil.toInorderList(root)
+    print(r)
+    assert(r == [])
+
+    root = TreeNodeUtil.fromBfsList([1])
+    r = TreeNodeUtil.toInorderList(root)
+    print(r)
+    assert(r == [1])
+
+    root = TreeNodeUtil.fromBfsList([1, 2])
+    r = TreeNodeUtil.toInorderList(root)
+    print(r)
+    assert(r == [2, 1])
+
+    root = TreeNodeUtil.fromBfsList([1, None, 2])
+    r = TreeNodeUtil.toInorderList(root)
+    print(r)
+    assert(r == [1, 2])
+
+    # test case
+    n1 = TreeNode(2)
+    n2 = TreeNode(5)
+    n3 = TreeNode(8)
+    n4 = TreeNode(6)
+    n5 = TreeNode(7)
+    n1.right = n2
+    n2.left = n3
+    n2.right = n4
+    n4.left = n5
+
+    nums = TreeNodeUtil.toBfsList(n1)
+    print(nums)
+    assert(nums == [2, None, 5, 8, 6, None, None, 7])
+
+    root = TreeNodeUtil.fromBfsList(nums)
+    nums = TreeNodeUtil.toBfsList(root)
+    print(nums)
+    assert(nums == [2, None, 5, 8, 6, None, None, 7])
