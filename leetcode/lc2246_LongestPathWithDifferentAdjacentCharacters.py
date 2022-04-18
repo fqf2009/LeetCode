b60219b8ff0acf1d668828f52de14f4d8e1c082a
FrankFeng <fqf2009@gmail.com>
2022-04-18 09:15:48 -0700

    BFS v.s. DFS

--- leetcode/lc2246_LongestPathWithDifferentAdjacentCharacters.py
+++ leetcode/lc2246_LongestPathWithDifferentAdjacentCharacters.py
@@ -53,6 +53,36 @@ class Solution:
 
 
 # Graph + BFS - T/S: O(n), O(n)
+# Optimize and simplify the code:
+# - keep max_len on the go; 
+# - only save extendable length in each node, just like DFS solution
+class Solution1:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        n = len(parent)
+        indegree = [0] * n
+        for p in parent:
+            if p != -1:
+                indegree[p] += 1
+
+        max_len = 0
+        path_len = [0] * n      # extendable_length for each node
+        dq = deque([i for i, v in enumerate(indegree) if v == 0])
+        while dq:
+            i = dq.popleft()
+            p = parent[i]
+
+            if s[i] != s[p]:
+                max_len = max(max_len, path_len[p] + path_len[i] + 1)
+                path_len[p] = max(path_len[p], path_len[i] + 1)
+
+            indegree[p] -= 1
+            if indegree[p] == 0 and p != 0: # do not add root node
+                dq.append(p)
+
+        return max_len + 1
+
+
+# Graph + BFS - T/S: O(n), O(n)
 # - no need to build graph, the parent list is graph;
 # - compute indegree for all nodes;
 # - BFS starting from leaf node, reduce indegree for its parent node
@@ -60,7 +90,7 @@ class Solution:
 # - distinguish extendable_path_len, un_extendable_path_len:
 #   if a node has different value with its child, then this path
 #   length can still be exended to its parent.
-class Solution1:
+class Solution2:
     def longestPath(self, parent: List[int], s: str) -> int:
         n = len(parent)
         indegree = [0] * n
@@ -97,7 +127,7 @@ if __name__ == "__main__":
     from unittest import TestCase, main
     from parameterized import parameterized, parameterized_class
 
-    @parameterized_class(('solution',), [(Solution,), (Solution1,)])
+    @parameterized_class(('solution',), [(Solution,), (Solution1,), (Solution2,)])
     class TestSolution(TestCase):
         @parameterized.expand([
             ([-1, 0, 0, 1, 1, 2], "abacbe", 3),
