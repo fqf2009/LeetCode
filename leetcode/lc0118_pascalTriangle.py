d5e75eb90d8033712c0b785e34dfdd748e566e44
FrankFeng <fqf2009@gmail.com>
2022-04-22 21:55:15 -0700

    Math

--- leetcode/lc0118_pascalTriangle.py
+++ leetcode/lc0118_pascalTriangle.py
@@ -1,27 +1,50 @@
+# Given an integer numRows, return the first numRows of Pascal's triangle.
+# In Pascal's triangle, each number is the sum of the two numbers 
+# directly above it as shown:
+# Constraints:
+#   1 <= numRows <= 30
+from typing import List
+
+
+# Iteration
 class Solution:
-    def generate(self, numRows: int) -> list[list[int]]:
-        if numRows == 1:
-            return [[1]]
-        res = self.generate(numRows - 1)
-        nums0 = res[numRows - 2]
-        nums1 = [nums0[0]]
-        for i in range(len(nums0) - 1):
-            nums1.append(nums0[i] + nums0[i+1])
-        nums1.append(nums0[-1])
-        res.append(nums1)
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]*(i+1) for i in range(numRows)]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                res[i][j] = res[i-1][j-1] + res[i-1][j]
+
         return res
 
 
-# test case
-if __name__ == "__main__":
-    numRows = 5
-    expected = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
-    r = Solution().generate(numRows)
-    print(r)
-    assert(r == expected)
+# Recursion
+class Solution1:
+    def generate(self, numRows: int) -> List[List[int]]:
+        n = numRows
+        if n == 1: return [[1]]
 
-    numRows = 1
-    expected = [[1]]
-    r = Solution().generate(numRows)
-    print(r)
-    assert(r == expected)
+        res = self.generate(n - 1)
+        prev_row = res[-1]
+        curr_row = [1] * n
+        for i in range(1, n-1):
+            curr_row[i] = prev_row[i-1] + prev_row[i]
+        res.append(curr_row)
+        return res
+
+
+if __name__ == "__main__":
+    from unittest import TestCase, main
+    from parameterized import parameterized, parameterized_class
+
+    @parameterized_class(('solution',), [(Solution,), (Solution1,)])
+    class TestSolution(TestCase):
+        @parameterized.expand([
+            (5, [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]),
+            (1, [[1]]),
+        ])
+        def test_generate(self, numRows, expected):
+            sol = self.solution()       # type:ignore
+            r = sol.generate(numRows)
+            self.assertEqual(r, expected)
+
+    main()
