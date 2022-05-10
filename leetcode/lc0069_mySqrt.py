4ac43d332366e6de9f09ec38c93b15e024e24e89
FrankFeng <fqf2009@gmail.com>
2022-05-09 19:55:30 -0700

    update

--- leetcode/lc0069_mySqrt.py
+++ leetcode/lc0069_mySqrt.py
@@ -1,7 +1,7 @@
 # Given a non-negative integer x, compute and return the square root of x.
-# Since the return type is an integer, the decimal digits are truncated, 
+# Since the return type is an integer, the decimal digits are truncated,
 # and only the integer part of the result is returned.
-# Note: You are not allowed to use any built-in exponent function or operator, 
+# Note: You are not allowed to use any built-in exponent function or operator,
 # such as pow(x, 0.5) or x ** 0.5.
 # Constraints:
 #   0 <= x <= 2^31 - 1
@@ -10,8 +10,10 @@
 # Binary search
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0: return 0
-        if x <= 3: return 1
+        if x == 0:
+            return 0
+        if x <= 3:
+            return 1
         left = 2
         right = x // 2
         while left <= right:
@@ -23,7 +25,7 @@ class Solution:
                 left = mid + 1
             else:
                 right = mid - 1
-        
+
         return right
 
 
@@ -32,33 +34,79 @@ class Solution1:
     def mySqrt(self, x):
         if x < 2:
             return x
-        
+
         x0 = x
         x1 = (x0 + x / x0) / 2
         while abs(x0 - x1) >= 1:
             x0 = x1
-            x1 = (x0 + x / x0) / 2        
-            
+            x1 = (x0 + x / x0) / 2
+
         return int(x1)
 
 
-if __name__ == '__main__':
-    r = Solution().mySqrt(4)
-    print(r)
-    assert(r == 2)
+# Math - Newton's formula
+# - for float type
+class Solution2:
+    def mySqrt(self, x: float) -> float:
+        def abs(x: float) -> float:
+            return x if x >= 0 else -x
 
-    r = Solution().mySqrt(6)
-    print(r)
-    assert(r == 2)
+        def isGoodEnough(guess: float, x: float) -> bool:
+            return abs(guess * guess - x) / x < 0.0000001
 
-    r = Solution().mySqrt(8)
-    print(r)
-    assert(r == 2)
+        def improve(guess: float, x: float):
+            return (guess + x / guess) / 2
 
-    r = Solution().mySqrt(100)
-    print(r)
-    assert(r == 10)
+        def sqrtIter(guess: float, x: float) -> float:
+            return guess if isGoodEnough(guess, x) else sqrtIter(improve(guess, x), x)
 
-    r = Solution().mySqrt(1000)
-    print(r)
-    assert(r == 31)
+        return sqrtIter(1.0, x)
+
+
+if __name__ == "__main__":
+
+    def unit_test(solution):
+        sol = solution()
+
+        r = sol.mySqrt(4)
+        print(r)
+        assert r == 2
+
+        r = sol.mySqrt(6)
+        print(r)
+        assert r == 2
+
+        r = sol.mySqrt(8)
+        print(r)
+        assert r == 2
+
+        r = sol.mySqrt(100)
+        print(r)
+        assert r == 10
+
+        r = sol.mySqrt(1000)
+        print(r)
+        assert r == 31
+
+    def unit_test2(solution):
+        sol = solution()
+
+        r = sol.mySqrt(64.0)
+        print(r)
+        assert abs(r - 8.0) < 0.000001
+
+        r = sol.mySqrt(1.0)
+        print(r)
+        assert abs(r - 1.0) < 0.000001
+
+        r = sol.mySqrt(10.0)
+        print(r)
+        assert abs(r - 3.1622776602) < 0.000001
+
+        r = sol.mySqrt(.5)
+        print(r)
+        assert abs(r - 0.7071067812) < 0.000001
+
+    unit_test(Solution)
+    unit_test(Solution1)
+    unit_test2(Solution2)
