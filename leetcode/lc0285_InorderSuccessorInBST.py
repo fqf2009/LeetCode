4a159fb211e18c5bdc566938f2cf09fd58e7a7b7
FrankFeng <fqf2009@gmail.com>
2022-05-28 18:55:03 -0700

    Tree

--- leetcode/lc0285_InorderSuccessorInBST.py
+++ leetcode/lc0285_InorderSuccessorInBST.py
@@ -1,12 +1,16 @@
-from lib.TreeUtil import TreeNode, TreeNodeUtil
-from typing import Optional, List
-
-# Given a binary search tree and a node in it, find the in-order successor of that node
-# in BST. The successor of a node p is the node with the smallest key greater than p.val.
-
+# Given a binary search tree and a node p in it, find the in-order successor 
+# of that node in BST. The successor of a node p is the node with the 
+# smallest key greater than p.val.
 # Note:
 #   If the given node has no in-order successor in the tree, return None.
 #   It's guaranteed that the values of the tree are unique.
+# Constraints:
+#   The number of nodes in the tree is in the range [1, 10^4].
+#   -10^5 <= Node.val <= 10^5
+#   All Nodes will have unique values.
+from lib.TreeUtil import TreeNode, TreeNodeUtil
+from typing import Optional, List
+
 
 # DFS + Recursion
 class Solution:
@@ -17,10 +21,7 @@ class Solution:
             return self.inorderSuccessor(root.right, p)
         else:
             node = self.inorderSuccessor(root.left, p)
-            if node == None:
-                return root
-            else:
-                return node
+            return node if node else root
 
 
 if __name__ == '__main__':
