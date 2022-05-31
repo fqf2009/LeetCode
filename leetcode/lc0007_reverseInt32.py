95a2099670ffc0d0bcb834c7371d161e993e8584
FrankFeng <fqf2009@gmail.com>
2022-05-31 11:14:12 -0700

    Math

--- leetcode/lc0007_reverseInt32.py
+++ leetcode/lc0007_reverseInt32.py
@@ -1,28 +1,45 @@
-# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes
-# the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
-# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+# Given a signed 32-bit integer x, return x with its digits reversed. 
+# If reversing x causes
+# the value to go outside the signed 32-bit integer range
+# [-2^31, 2^31 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit 
+# integers (signed or unsigned).
+
 
 class Solution:
     def reverse(self, x: int) -> int:
         res = int(str(x)[::-1]) if x >= 0 else -self.reverse(-x)
-        if res > 2**31 - 1 or res < -(2**31):
-            return 0
-        return res
+        return res if -(2**31) <= res < 2**31 - 1 else 0
 
 
 class Solution1:
     def reverse(self, x: int) -> int:
-        if x < 0:
-            return -self.reverse(-x)
-            
+        if x < 0: return -self.reverse(-x)
+
         res = 0
         while x > 0:
             x, r = divmod(x, 10)
             res = res * 10 + r
         
-        if res > 2**31 - 1 or res < -(2**31):
-            return 0
-        
+        return res if -(2**31) <= res < 2**31 - 1 else 0
+
+
+# If only 32-bit integer is allowed?
+# - Python's int does not have any limit on precision
+# - divide/mod on negative int: Python is different from Java
+# - Python: -123 // 10 == -13, -123 % 10 == 7
+# - Java:   -123 // 10 == -12, -123 % 10 == -3
+class Solution2:
+    def reverse(self, x: int) -> int:
+        if x < 0: return -self.reverse(-x)
+        res = 0
+        while x != 0:
+            x, r = divmod(x, 10)
+            res1 = res * 10 + r
+            if res1 // 10 != res:   # now working, no int overflow in python
+                return 0
+            res = res1
+
         return res
 
 
@@ -40,13 +57,14 @@ if __name__ == '__main__':
         print(n)    
         assert n == 21
 
-        n = sol.reverse(8463847412)
+        n = sol.reverse(2147483647)
         print(n)
         assert n == 0
 
-        n = sol.reverse(-8463847412)
+        n = sol.reverse(-2147483648)
         print(n)
         assert n == 0
 
     unitTest(Solution())
     unitTest(Solution1())
+    # unitTest(Solution2())
