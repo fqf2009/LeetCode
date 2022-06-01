e7f90a2021fbbc4d3fc57504f843af4e3afbca82
FrankFeng <fqf2009@gmail.com>
2022-05-31 20:31:42 -0700

    Permutation

--- leetcode/lc0031_nextPermutation.py
+++ leetcode/lc0031_nextPermutation.py
@@ -1,6 +1,6 @@
-# Implement next permutation, which rearranges numbers into the lexicographically 
+# Implement next permutation, which rearranges numbers into the lexicographically
 # next greater permutation of numbers.
-# If such an arrangement is impossible, it must rearrange it to the lowest possible 
+# If such an arrangement is impossible, it must rearrange it to the lowest possible
 # order (i.e., sorted in ascending order).
 # The replacement must be in place and use only constant extra memory.
 # Constraints:
@@ -11,10 +11,10 @@ from typing import List
 
 # Analysis:
 #  - what does this next permutation mean? greater in one-by-one field comparison.
-#  - e.g., [3,2,1] (Ever Decreasing Series) has no next permutation, only to 
+#  - e.g., [3,2,1] (Ever Decreasing Series) has no next permutation, only to
 #          reverse to minimum permutation [1,2,3]
 #  - e.g., [8,9,6,10,7,2], the [10,7,2] portion has no next greater permutation. So
-#          find the 6 (pivot) where EDS stops, then find the minimum number in EDS but 
+#          find the 6 (pivot) where EDS stops, then find the minimum number in EDS but
 #          than 6, it is 7. Now swap 6 and 7, we get [8,9,7,10,6,2], which is greater
 #          greater than original permutation, and the last portion [10,6,2] is still EDS.
 #          if we reverse this EDS, we get [2,6,10], this is the minimum for this portion.
@@ -22,13 +22,13 @@ from typing import List
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         n = len(nums)
-        p = 0   # pos to reverse array
-        for i in reversed(range(n-1)):
-            if nums[i] < nums[i + 1]:   # i is pivot pos
+        p = 0  # pos to reverse array
+        for i in reversed(range(n - 1)):
+            if nums[i] < nums[i + 1]:  # i is pivot pos
                 k = n - 1
-                for j in reversed(range(i+1, n)):
+                for j in reversed(range(i + 1, n)):
                     if nums[j] > nums[i]:
-                        k = j           # k is swap pos
+                        k = j  # k is swap pos
                         break
                 nums[i], nums[k] = nums[k], nums[i]
                 p = i + 1
@@ -36,39 +36,52 @@ class Solution:
 
         # reverse
         for i in range(p, p + (n - p) // 2):
-            nums[i], nums[-1 - (i-p)] = nums[-1 - (i-p)], nums[i]
+            nums[i], nums[-1 - (i - p)] = nums[-1 - (i - p)], nums[i]
+
+
+# more pythonic
+class Solution1:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+        p = next((i for i in reversed(range(n - 1)) if nums[i] < nums[i + 1]), -1)
+        if p >= 0:
+            k = next((i for i in reversed(range(p + 1, n)) if nums[p] < nums[i]))
+            nums[p], nums[k] = nums[k], nums[p]
+        nums[p + 1 :] = nums[p + 1 :][::-1]
 
 
 if __name__ == "__main__":
+
     def unitTest(sol):
-        nums = [1,3,2]
+        nums = [1, 3, 2]
         sol.nextPermutation(nums)
         print(nums)
-        assert(nums == [2,1,3])
+        assert nums == [2, 1, 3]
 
-        nums = [8,9,6,10,7,2]
+        nums = [8, 9, 6, 10, 7, 2]
         print(nums)
         sol.nextPermutation(nums)
-        assert(nums == [8,9,7,2,6,10])
+        assert nums == [8, 9, 7, 2, 6, 10]
 
-        nums = [1,2,3]
+        nums = [1, 2, 3]
         print(nums)
         sol.nextPermutation(nums)
-        assert(nums == [1,3,2])
+        assert nums == [1, 3, 2]
 
-        nums = [3,2,1]
+        nums = [3, 2, 1]
         print(nums)
         sol.nextPermutation(nums)
-        assert(nums == [1,2,3])
+        assert nums == [1, 2, 3]
 
-        nums = [1,1,5]
+        nums = [1, 1, 5]
         print(nums)
         sol.nextPermutation(nums)
-        assert(nums == [1,5,1])
+        assert nums == [1, 5, 1]
 
         nums = [1]
         print(nums)
         sol.nextPermutation(nums)
-        assert(nums == [1])
+        assert nums == [1]
 
     unitTest(Solution())
+    unitTest(Solution1())
