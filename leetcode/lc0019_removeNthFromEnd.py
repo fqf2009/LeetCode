ee58ec21b854b15ccbd034331fd79fd87f4a4539
FrankFeng <fqf2009@gmail.com>
2022-05-31 20:32:21 -0700

    Linked List

--- leetcode/lc0019_removeNthFromEnd.py
+++ leetcode/lc0019_removeNthFromEnd.py
@@ -1,8 +1,12 @@
-from lib.ListUtil import ListNode, ListNodeUtil
-from typing import Optional
-
 # Given the head of a linked list, remove the nth node from the end of 
 # the list and return its head.
+# Constraints:
+#   The number of nodes in the list is sz.
+#   1 <= sz <= 30
+#   0 <= Node.val <= 100
+#   1 <= n <= sz
+from lib.ListUtil import ListNode, ListNodeUtil
+from typing import Optional
 
 
 # Scan twice: time O(n), space O(1)
