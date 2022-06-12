a53dbf44a14e4f5ca668363f7ee615383c538c15
FrankFeng <fqf2009@gmail.com>
2022-06-12 16:03:53 -0700

    Design

--- leetcode/lc0297_SerializeAndDeserializeBinaryTree.py
+++ leetcode/lc0297_SerializeAndDeserializeBinaryTree.py
@@ -3,14 +3,14 @@
 # file or memory buffer, or transmitted across a network connection
 # link to be reconstructed later in the same or another computer
 # environment.
-# Design an algorithm to serialize and deserialize a binary tree. 
-# There is no restriction on how your serialization/deserialization 
-# algorithm should work. You just need to ensure that a binary tree 
+# Design an algorithm to serialize and deserialize a binary tree.
+# There is no restriction on how your serialization/deserialization
+# algorithm should work. You just need to ensure that a binary tree
 # can be serialized to a string and this string can be deserialized
 # to the original tree structure.
-# Clarification: The input/output format is the same as how LeetCode 
-# serializes a binary tree. You do not necessarily need to follow 
-# this format, so please be creative and come up with different 
+# Clarification: The input/output format is the same as how LeetCode
+# serializes a binary tree. You do not necessarily need to follow
+# this format, so please be creative and come up with different
 # approaches yourself.
 # Constraints:
 #   The number of nodes in the tree is in the range [0, 10^4].
@@ -22,6 +22,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
 from lib.TreeUtil import TreeNode, TreeNodeUtil
 
 
@@ -32,29 +33,30 @@ class Codec:
         :rtype: str
         """
         res = []
+
         def dfs_serialize(root):
             if not root:
-                res.append('None')
+                res.append("None")
             else:
                 res.append(str(root.val))
                 dfs_serialize(root.left)
                 dfs_serialize(root.right)
-        
-        dfs_serialize(root)
-        return ','.join(res)
 
+        dfs_serialize(root)
+        return ",".join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         :type data: str
         :rtype: TreeNode
         """
-        lst = data.split(',')
+        lst = data.split(",")
 
         def dfs_deserialize(i):
-            if i == len(lst): return (None, i)
+            if i == len(lst):
+                return (None, i)
 
-            if lst[i] == 'None':
+            if lst[i] == "None":
                 return (None, i + 1)
             else:
                 root = TreeNode(int(lst[i]))
@@ -65,24 +67,56 @@ class Codec:
         return dfs_deserialize(0)[0]
 
 
-# Your Codec object will be instantiated and called as such:
+class Codec1:
+    def serialize(self, root):
+        res = []
+
+        def dfs_serialize(root):
+            if not root:
+                res.append("None")
+            else:
+                res.append(str(root.val))
+                dfs_serialize(root.left)
+                dfs_serialize(root.right)
+
+        dfs_serialize(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        def dfs_deserialize(dq):
+            if dq[0] == "None":
+                dq.popleft()
+                return None
+            else:
+                root = TreeNode(int(dq.popleft()))
+                root.left = dfs_deserialize(dq)
+                root.right = dfs_deserialize(dq)
+                return root
+
+        return dfs_deserialize(deque(data.split(",")))
 
 
 if __name__ == "__main__":
     from unittest import TestCase, main
     from parameterized import parameterized, parameterized_class
 
-    @parameterized_class(('codec',), [(Codec,)])
+    @parameterized_class(("codec",), [(Codec,), (Codec1,)])
     class TestSolution(TestCase):
-        @parameterized.expand([
-            ([1,2,3,None,None,4,5], [1,2,3,None,None,4,5]),
-            ([], []),
-        ])
+        @parameterized.expand(
+            [
+                ([1, 2, 3, None, None, 4, 5], [1, 2, 3, None, None, 4, 5]),
+                ([], []),
+            ]
+        )
         def test_maximumProduct(self, bfs_lst, expected):
             root = TreeNodeUtil.fromBfsList(bfs_lst)
-            ser = self.codec()       # type: ignore
-            deser = self.codec()     # type: ignore
-            root2 = deser.deserialize(ser.serialize(root))
+            print("")
+            print(self.codec.__name__)  # type: ignore
+            ser = self.codec()          # type: ignore
+            deser = self.codec()        # type: ignore
+            r = ser.serialize(root)
+            print(r)
+            root2 = deser.deserialize(r)
             bfs_lst2 = TreeNodeUtil.toBfsList(root2)
             self.assertEqual(bfs_lst2, expected)
 
