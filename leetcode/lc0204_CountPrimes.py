d842c28dccce59db08d720b8fc0d76e9c78ed328
FrankFeng <fqf2009@gmail.com>
2022-06-14 21:01:31 -0700

    Math

--- leetcode/lc0204_CountPrimes.py
+++ leetcode/lc0204_CountPrimes.py
@@ -1,8 +1,9 @@
-# Given an integer n, return the number of prime numbers that are 
+# Given an integer n, return the number of prime numbers that are
 # strictly less than n.
 # Constraints:
 #   0 <= n <= 5 * 10^6
 import math
+
 
 class Solution:
     def countPrimes(self, n: int) -> int:
@@ -16,18 +17,32 @@ class Solution:
         return isPrime[2:].count(True)
 
 
+def allPrimes(n) -> list[int]:
+    isPrime = [True] * (n + 1)
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if isPrime[i]:
+            for j in range(i * i, n + 1, i):
+                isPrime[j] = False
+                j += 1
+
+    return [i for i in range(2, n + 1) if isPrime[i]]
+
+
 if __name__ == "__main__":
+
     def unit_test(sol):
         r = sol.countPrimes(10)
         print(r)
-        assert(r == 4)
+        assert r == 4
 
         r = sol.countPrimes(20)
         print(r)
-        assert(r == 8)
+        assert r == 8
 
-        r = sol.countPrimes(5*10**6)
-        print(r)
-        assert(r == 348513)
-    
+        # r = sol.countPrimes(5*10**6)
+        # print(r)
+        # assert(r == 348513)
+
     unit_test(Solution())
+
+    print(allPrimes(100))
