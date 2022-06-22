1bdfee4bbfda159815e8a61aa0e5a0ef461abf0d
FrankFeng <fqf2009@gmail.com>
2022-06-22 10:43:46 -0700

    Queue + Stack

---
+++ leetcode/lc0232_ImplementQueueUsingStacks.py
@@ -0,0 +1,57 @@
+# Implement a first in first out (FIFO) queue using only two stacks. 
+# The implemented queue should support all the functions of a normal
+# queue (push, peek, pop, and empty).
+# Implement the MyQueue class:
+#   void push(int x) Pushes element x to the back of the queue.
+#   int pop() Removes the element from the front of the queue and returns it.
+#   int peek() Returns the element at the front of the queue.
+#   boolean empty() Returns true if the queue is empty, false otherwise.
+# Notes:
+# - You must use only standard operations of a stack, which means only 
+#   push to top, peek/pop from top, size, and is empty operations are valid.
+# - Depending on your language, the stack may not be supported natively. You may 
+#   simulate a stack using a list or deque (double-ended queue) as long as you 
+#   use only a stack's standard operations.
+# Constraints:
+#   1 <= x <= 9
+#   At most 100 calls will be made to push, pop, peek, and empty.
+#   All the calls to pop and peek are valid.
+# Follow-up: Can you implement the queue such that each operation is 
+# amortized O(1) time complexity? In other words, performing n operations 
+# will take overall O(n) time even if one of those operations may take longer.
+
+
+# stack1 for push() - O(1) for consecutive call, O(n) after pop()
+# stack2 for pop()  - O(1) for consecutive call, O(n) after push()
+# peek(), empty() - O(1)
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if self.stack1:
+            return self.stack1[0]
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
