96c677602dcfd2bceb092f2104f9eabfae3712f0
FrankFeng <fqf2009@gmail.com>
2022-06-29 12:20:40 -0700

    DP

--- leetcode/lc0416_PartitionEqualSubsetSum.py
+++ leetcode/lc0416_PartitionEqualSubsetSum.py
@@ -10,17 +10,32 @@ from typing import List
 
 
 # DP + Recursion + Memo
-# - think about: sum of subsequence equal to half of the sum of array
+# - sum of subsequence equal to half of the sum of array
+# - calling dp(n-1, k - nums[n-1]) before calling dp(n-1, k)
+#   seems much faster.
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         @cache
         def dp(n: int, k: int) -> bool:
-            if n == 0: return k == 0
-            return dp(n-1, k) or dp(n-1, k - nums[n-1])
+            if n == 0 or k <= 0: return k == 0      # k <= 0 !!! critical to prune pathes
+            return dp(n-1, k - nums[n-1]) or dp(n-1, k)  # invoking order is more important?
 
         total = sum(nums)
         if total % 2 != 0: return False
         return dp(len(nums), total // 2)
+
+
+class Solution1:
+    def canPartition(self, nums: List[int]) -> bool:
+        @cache
+        def dp(i: int, remain: int) -> bool:
+            if i == len(nums) or remain <= 0:   # remain <= 0 is critical to prune pathes!!!
+                return remain == 0
+            return dp(i+1, remain - nums[i]) or dp(i+1, remain)
+
+        target = sum(nums)
+        if target % 2 != 0: return False
+        return dp(0, target // 2)
 
 
 if __name__ == '__main__':
@@ -34,3 +49,4 @@ if __name__ == '__main__':
         assert r == False
 
     unitTest(Solution())
+    unitTest(Solution1())
