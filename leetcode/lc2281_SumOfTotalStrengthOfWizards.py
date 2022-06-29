5e647cf925391c89e10b0884b1656917dec9325b
FrankFeng <fqf2009@gmail.com>
2022-06-29 12:46:18 -0700

    Wizard Power (prefix-prefix-sum)

--- leetcode/lc2281_SumOfTotalStrengthOfWizards.py
+++ leetcode/lc2281_SumOfTotalStrengthOfWizards.py
@@ -17,6 +17,54 @@ from typing import List
 
 # T/S: O(n), O(n)
 # From: https://leetcode.com/problems/sum-of-total-strength-of-wizards/discuss/2061985/Python-Solution-O(n)
+# Better Explain: https://leetcode.com/problems/sum-of-total-strength-of-wizards/discuss/2062017/C%2B%2B-prefix-%2B-monotonic-stack-O(N)-solution-with-thought-process
+# related: monotonic stack
+#   0496_NextGreaterElementI 
+#   0503_NextGreaterElementII
+# Algorithm
+# - For each strength[i], we could find a non-empty index range (left, right) 
+#   where strength[i] is the min value. So for all subarrays in this range 
+#   including strength[i], the total strength is strength[i] * the sum of
+#   those subarray sums.
+# - left is the first index on the left side i where strength[left] < strength[i]
+# - right is the first index on the right side of i where strength[right] <= strength[i]
+# - These two indices can be pre-calculated using monotonic stack.
+# - The reason we use < on left but <= on right is to avoid duplicates.
+#   Here is an example array: 1 (2 3 4 2) 3 4 2 1
+#   For the highlighted subarray 2 3 4 2, we want to calculate the strength using 
+#   the 2nd 2 but not the first 2.
+# - How do we get the "sum of all subarrays including strength[i] in range (left, right)"?
+#   - Let's list the indices:
+#       ...left-1, left, left + 1, left + 2, ... i-1, i, i+1, ... right-1, right, right+1...
+#   - Let prefix[i] be the prefix sum of first i elements in strength.
+#   - The sum of subarrays including i are:
+#     - the subarrays that start with left+1, end with i, i+1, right-1
+#         sum(left+1, ... i) = prefix[i + 1] - prefix[left + 1]
+#         sum(left+1, ... i+1) = prefix[i + 2] - prefix[left + 1]
+#         ...
+#         sum(left+1, ... right-1) = prefix[right] - prefix[left + 1]
+#     - the subarrays that start with left+2, end with i, i+1, right-1
+#         sum(left+2, ... i) = prefix[i + 1] - prefix[left + 2]
+#         sum(left+2, ... i+1) = prefix[i + 2] - prefix[left + 2]
+#         ...
+#         sum(left+2, ... right-1) = prefix[right] - prefix[left + 2]
+#         ...
+#     - the subarrays that start with i, end with i, i+1, right-1
+#         sum(i, ... i) = prefix[i + 1] - prefix[i]
+#         sum(i, ... i+1) = prefix[i + 2] - prefix[i]
+#         ...
+#         sum(i, ... right-1) = prefix[right] - prefix[i]
+#   - Then we combine all above terms, we have:
+#     - positive parts:
+#         (prefix[i + 1] + prefix[i + 2] + ... + prefix[right]) * (i - left)
+#     - negative parts:
+#         (prefix[left + 1] + prefix[left + 2] + ... + prefix[i]) * (right - i)
+# - The range sum of prefix can be optimized by pre-computing prefix-sum of prefix.
+#     - let acc = prefix-sum(prefix)
+#     - positive parts:
+#         (acc[right] - acc[i]) * (i - left)
+#     - negative parts:
+#         (acc[i] - acc[left]) * (right - i)
 class Solution:
     def totalStrength(self, strength: List[int]) -> int:
         A = strength
@@ -26,8 +74,8 @@ class Solution:
         # position of next smaller on the right, or, n
         right = [n] * n
         stack = []  # mono-increasing stack
-        for i in range(n):
-            while stack and A[stack[-1]] > A[i]:
+        for i, v in enumerate(A):
+            while stack and A[stack[-1]] > v:
                 right[stack.pop()] = i
             stack.append(i)
 
